051ec7c5a76c24d637ffa42bf70488a9bc2a8cd7
Eduardo Mosko <eduardommosko@gmail.com>
2022-01-30 18:29:38 -0300

    MainPage lists actual posts

--- pages/index.js
+++ pages/index.js
@@ -1,37 +1,50 @@
+import { promises as fs } from 'fs';
+import path from 'path';
 import Card from "../components/Card";
+import A from '../components/Link';
 
-const cards = [
-  {
-    title: "Hello, World",
-    description: "Lorem impsum dolor sit amed.",
-  },
-  {
-    title: "Hello, World",
-    description: "Lorem impsum dolor sit amed.",
-  },
-  {
-    title: "Hello, World",
-    description: "Lorem impsum dolor sit amed.",
-  },
-]
-
-const Home = () => (
-  <>
-    <div className="grid grid-cols-1 md:grid-cols-2 2xl:grid-cols-3">
-      {
-        cards.map((c, i) => (
-          <>
-            <Card {...c} key={i} />
-            <Card {...c} key={i} />
-            <Card {...c} key={i} />
-            <Card {...c} key={i} />
-            <Card {...c} key={i} />
-            <Card {...c} key={i} />
-          </>
-        ))
-      }
-    </div>
-  </>
+const Home = ({ posts }) => (
+  <div className="grid grid-cols-1 md:grid-cols-2 2xl:grid-cols-3">
+    {
+      posts.map((post) => (
+        //<A key={post.link} href={`./${post.link}`}>
+          <Card title={post.title} description={post.description} key={post.link}/>
+        //</A>
+      ))
+    }
+  </div>
 )
 
+const getAllPosts = async () => {
+  const allDir = path.join(process.cwd(), 'posts');
+  const years = await fs.readdir(allDir);
+  const posts = [];
+
+  for (const y of years) {
+    console.log(y);
+    const yearDir = path.join(allDir, y);
+    const months = await fs.readdir(yearDir);
+
+    for (const m of months) {
+      const monthDir = path.join(yearDir, m);
+      const postsDir = await fs.readdir(monthDir);
+
+      for (const p of postsDir) {
+        posts.push({
+          link: p,
+          title: p,
+          description: 'Lorem ipsum dolor sit amet.'
+        })
+      }
+    }
+  }
+
+  return posts.reverse();
+};
+
+const getStaticProps = async () => ({
+  props: { posts: await getAllPosts(), },
+});
+
 export default Home;
+export { getStaticProps };
